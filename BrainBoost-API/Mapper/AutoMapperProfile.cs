c1c6dd5b5808014c9108ccf500c17ea3da68f422
PoulaMaher <poula.maher199@gmail.com>
2024-05-31 16:26:13 +0300

    payment Endpoints

---
+++ BrainBoost-API/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BrainBoost_API.DTOs.Subscription;
+using BrainBoost_API.Models;
+
+namespace BrainBoost_API.Mapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<SubscriptionDto, subscription>().ReverseMap();
+
+        }
+    }
+}
